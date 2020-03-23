198a9f423902d7323531388e9a99d6e86b84c244
MarcelloVSilva <marcellovcs@gmail.com>
2020-03-23 14:36:09 -0300

    enhance: query post by author with pagination

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {useEffect} from 'react';
-import {StatusBar, Button, Dimensions, View} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import {StatusBar, Button, Dimensions, View, Text} from 'react-native';
 import {
   createPost,
   deletePost,
@@ -17,13 +17,28 @@ import {
   reactPost,
   registerUser,
 } from './src/db/soa';
+import {PostsByAuthor} from './src/db/query';
 
 const App = () => {
+  const postIdTest = '4fzmz5NtAXm49x2BPH1r';
+
+  const postByAuthorRef = new PostsByAuthor();
+  const [postsByAuthor, setPostsByAuthor] = useState([]);
+
   useEffect(() => {
     registerUser();
-  }, []);
+    fetchPosts();
+  }, [fetchPosts]);
 
-  const postIdTest = '4fzmz5NtAXm49x2BPH1r';
+  const fetchPosts = useCallback(async () => {
+    const docs = await postByAuthorRef.get();
+    setPostsByAuthor(docs);
+  }, [postByAuthorRef]);
+
+  const fetchMorePosts = async () => {
+    const docs = await postByAuthorRef.fetchMore();
+    setPostsByAuthor([...postsByAuthor, ...docs]);
+  };
 
   const handlePressCreatePost = async () => {
     try {
@@ -83,11 +98,17 @@ const App = () => {
           height: Dimensions.get('window').height,
         }}>
         <Button title="Create post" onPress={handlePressCreatePost} />
-        <Button title="Delete post" onPress={handlePressDeletePost} />
+        <View>
+          {postsByAuthor.map(post => {
+            return <Text>{post.data().message}</Text>;
+          })}
+        </View>
+        <Button title="Fetch more posts" onPress={fetchMorePosts} />
+        {/* <Button title="Delete post" onPress={handlePressDeletePost} />
         <Button title="Favor post" onPress={handlePressFavorPost} />
         <Button title="Disfavor post" onPress={handlePressDisfavorPost} />
         <Button title="Ban post" onPress={handlePressBanPost} />
-        <Button title="React post" onPress={handlePressReactPost} />
+        <Button title="React post" onPress={handlePressReactPost} /> */}
       </View>
     </>
   );
--- src/db/query/index.js
+++ src/db/query/index.js
@@ -1,11 +1,46 @@
-export function getPost(postId) {}
+import {firebase} from '@react-native-firebase/firestore';
+import {getCreatedBy} from '../../utils';
 
+export function getPost(postId) {
+  return firebase
+    .firestore()
+    .collection('posts')
+    .doc(postId)
+    .get();
+}
+
+let lastDocFetched = null;
 export class PostsByAuthor {
-  authorId = '';
-  lastPostId = '';
+  createdBy = getCreatedBy();
 
-  get() {}
-  fetchMore() {}
+  get() {
+    return new Promise(async (resolve, reject) => {
+      const ref = firebase
+        .firestore()
+        .collection('posts')
+        .where('createdBy', '==', this.createdBy)
+        .limit(1);
+
+      const ret = await ref.get();
+      lastDocFetched = ret.docs[ret.docs.length - 1];
+      resolve(ret.docs);
+    });
+  }
+
+  fetchMore() {
+    return new Promise(async (resolve, reject) => {
+      const ref = firebase
+        .firestore()
+        .collection('posts')
+        .where('createdBy', '==', this.createdBy)
+        .startAfter(lastDocFetched)
+        .limit(1);
+
+      const ret = await ref.get();
+      lastDocFetched = ret.docs[ret.docs.length - 1];
+      resolve(ret.docs);
+    });
+  }
 }
 
 export class AllPosts {
